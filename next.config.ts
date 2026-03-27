ce29c441f9d23dbee094e7aa23ffadcce13bd144
Md Rakibul Alam <hrmrakibs@gmail.com>
2026-03-27 16:18:14 +0600

    message works

--- next.config.ts
+++ next.config.ts
@@ -4,23 +4,21 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
         protocol: "http",
         hostname: "10.10.12.49",
-        port: "8002",
+        port: "8000",
         pathname: "/media/**",
       },
       {
         protocol: "http",
         hostname: "69.62.116.203",
         port: "8000",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "69.62.116.203",
+        port: "8002",
         pathname: "/media/**",
       },
     ],
---
+++ src/redux/features/messages/messagesAPI.ts
@@ -0,0 +1,49 @@
+import baseAPI from "@/redux/api/api";
+
+const messagesAPI = baseAPI.injectEndpoints({
+  endpoints: (builder) => ({
+    createConversation: builder.mutation({
+      query: (body) => ({
+        url: "/chat/conversations/",
+        method: "POST",
+        body,
+      }),
+    }),
+
+    getMyConversations: builder.query({
+      query: ({ page, limit, search, message_status }) => ({
+        url: `/chat/conversations/`,
+        method: "GET",
+        params: { page, limit, search, message_status },
+      }),
+    }),
+
+    // &search=${search || ""}&read=${read || ""}&unRead=${unRead || ""}
+
+    getMessages: builder.query({
+      query: ({ conversationId, page, limit, search }) => ({
+        url: `/chat/conversations/${conversationId}/messages/`,
+        method: "GET",
+        params: { page, limit },
+      }),
+    }),
+
+    fileUploadWithMessage: builder.mutation({
+      query: ({ conversationId, body }) => {
+        return {
+          url: `/chat/conversations/${conversationId}/upload/`,
+          method: "POST",
+          body,
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useCreateConversationMutation,
+  useGetMyConversationsQuery,
+  useGetMessagesQuery,
+  useFileUploadWithMessageMutation,
+} = messagesAPI;
+export default messagesAPI;
